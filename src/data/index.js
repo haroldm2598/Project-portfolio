4db845a936ac1eabac2c6db1e4493258e286e0c5
Harold <cvuyante24@gmail.com>
2023-08-15 21:12:14 +0800

    image fix

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,8 @@
+import image1 from '../assets/image/todoList.png';
+import image2 from '../assets/image/resume.png';
+import image3 from '../assets/image/memoryGame.png';
+import image4 from '../assets/image/ecommerce.png';
+
 export const projectObjects = [
 	{
 		title: 'Todo List',
@@ -11,7 +16,7 @@ export const projectObjects = [
 			'npm'
 		],
 		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ullam perspiciatis doloremque dolorum ad quis accusamus nobis, accusantium incidunt exercitationem facilis. Libero temporibus, porro quaerat tenetur reprehenderit consequatur numquam incidunt',
-		image: '/src/assets/image/todolist.png',
+		image: image1,
 		orderImage: 2,
 		orderInfo: 1,
 		orderImagePosition: 1000,
@@ -21,7 +26,7 @@ export const projectObjects = [
 		title: 'Resume Builder',
 		techStack: ['html', 'css', 'sass', 'javascript', 'react', 'github', 'npm'],
 		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ullam perspiciatis doloremque dolorum ad quis accusamus nobis, accusantium incidunt exercitationem facilis. Libero temporibus, porro quaerat tenetur reprehenderit consequatur numquam incidunt',
-		image: '/src/assets/image/resume.png',
+		image: image2,
 		orderImage: 1,
 		orderInfo: 2,
 		orderImagePosition: -1000,
@@ -31,7 +36,7 @@ export const projectObjects = [
 		title: 'Memory Game',
 		techStack: ['html', 'css', 'sass', 'javascript', 'react', 'github', 'npm'],
 		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ullam perspiciatis doloremque dolorum ad quis accusamus nobis, accusantium incidunt exercitationem facilis. Libero temporibus, porro quaerat tenetur reprehenderit consequatur numquam incidunt',
-		image: '/src/assets/image/memoryGame.png',
+		image: image3,
 		orderImage: 2,
 		orderInfo: 1,
 		orderImagePosition: 1000,
@@ -50,7 +55,7 @@ export const projectObjects = [
 			'npm'
 		],
 		info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ullam perspiciatis doloremque dolorum ad quis accusamus nobis, accusantium incidunt exercitationem facilis. Libero temporibus, porro quaerat tenetur reprehenderit consequatur numquam incidunt',
-		image: '/src/assets/image/ecommerce.png',
+		image: image4,
 		orderImage: 1,
 		orderInfo: 2,
 		orderImagePosition: -1000,
